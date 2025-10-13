611aa5f31edf1fc6883b000fe4239dac56242cd6
xxxMichael <mayckolchavez27@gmail.com>
2025-10-13 12:37:29 -0500

    Agregar página de Marketplace con funcionalidad de filtros, paginación y carga de productos

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import RegisterPage from "./pages/RegisterPage";
 import VerifyEmailPage from "./pages/VerifyEmailPage";
 import AdminCreateModeratorPage from "./pages/AdminCreateModeratorPage";
 import WelcomePage from "./pages/WelcomePage";
+import MarketplacePage from "./pages/MarketplacePage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import RecoverPasswordPage from "./pages/RecoverPasswordPage";
 import ResetPasswordPage from "./pages/ResetPasswordPage";
@@ -13,7 +14,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/marketplace" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/verify-email" element={<VerifyEmailPage />} />
@@ -21,6 +22,8 @@ export default function App() {
         <Route path="/recover-password" element={<RecoverPasswordPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
 
+        {/* Marketplace - Accesible sin autenticación */}
+        <Route path="/marketplace" element={<MarketplacePage />} />
 
         {/* Rutas protegidas */}
         <Route
@@ -33,7 +36,7 @@ export default function App() {
         />
 
         {/* 404 */}
-        <Route path="*" element={<Navigate to="/login" replace />} />
+        <Route path="*" element={<Navigate to="/marketplace" replace />} />
       </Routes>
     </BrowserRouter>
   );
